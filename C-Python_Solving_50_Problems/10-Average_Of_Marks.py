74ac803d8f423d5611fac91c6cb383718c711076
AdhamKhaledDev <adhamk109@outlook.com>
2024-01-21 18:07:05 -0800

    Average

---
+++ C-Python_Solving_50_Problems/10-Average_Of_Marks.py
@@ -0,0 +1,11 @@
+# Python program to calculate the average of three numbers
+# take inputs
+mark1 = float(input('Enter first mark: '))
+mark2 = float(input('Enter second mark: '))
+mark3 = float(input('Enter third mark: '))
+
+# calculate average
+avg = (mark1 + mark2 + mark3) / 3
+
+# display result
+print('The average of marks {0}, {1}, and {2} is {3:.2f}'.format(mark1, mark2, mark3, avg))
